dd3b9a61cca84eccfa347a39e3e62b3613857770
Julian Schumacher <Jules.media@outlook.de>
2023-03-15 10:59:34 +0100

    Add new Components

--- src/benutzerschnitstelle/Dashboard.java
+++ src/benutzerschnitstelle/Dashboard.java
@@ -21,27 +21,41 @@ public class Dashboard extends JPanel
 		// Pane Content
 		setBorder(new EmptyBorder(5, 5, 5, 5));
 		setMinimumSize(new Dimension(1000, 1000));
-		constraints.fill = GridBagConstraints.HORIZONTAL;
-		constraints.insets = new Insets(5, 5, 5, 5);
+		constraints.fill = GridBagConstraints.BOTH;
+		constraints.anchor = GridBagConstraints.FIRST_LINE_START;
 		layout.setConstraints(this, constraints);
 		setLayout(layout);
 		constraints.gridx = 0;
 		constraints.gridy = 0;
-		//constraints.gridwidth = 7;
-		//constraints.gridheight = 1;
+		constraints.gridwidth = 7;
+		constraints.gridheight = 1;
 		final JScrollPane statsScrollPane = new JScrollPane(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS, ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER);
 		statsScrollPane.setWheelScrollingEnabled(true);
 		addComponent(statsScrollPane);
 		constraints.gridx = 1;
 		constraints.gridy = 1;
-		//constraints.gridwidth = 2;
+		constraints.gridwidth = 2;
 		final JButton abfrageBtn = new JButton();
 		abfrageBtn.setText("Abfrage starten");
 		addComponent(abfrageBtn);
 		constraints.gridx = 5;
 		final JButton vokabelErstellerBtn = new JButton();
 		vokabelErstellerBtn.setText("Vokabel erstellen");
-		addComponent(abfrageBtn);
+		addComponent(vokabelErstellerBtn);
+		constraints.gridx = 0;
+		constraints.gridy = 2;
+		constraints.gridwidth = 7;
+		final JPanel letzteKategorienPanel = new JPanel();
+		letzteKategorienPanel.setBackground(Color.GRAY);
+		letzteKategorienPanel.add(new JLabel("Letzte Kategorien"));
+		addComponent(letzteKategorienPanel);
+		constraints.gridx = 0;
+		constraints.gridy = 3;
+		constraints.gridwidth = 7;
+		final JPanel alleKategorienPanel = new JPanel();
+		alleKategorienPanel.setBackground(Color.GRAY);
+		alleKategorienPanel.add(new JLabel("Alle Kategorien"));
+		addComponent(alleKategorienPanel);
 	}
 
 	private void addComponent(Component component) {
