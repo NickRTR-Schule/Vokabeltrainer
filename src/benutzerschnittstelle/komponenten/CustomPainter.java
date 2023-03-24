937b3f342f96c30cbabe3ce76198c9bc72204059
Julian Schumacher <Jules.media@outlook.de>
2023-03-24 11:51:59 +0100

    Add Charts

---
+++ src/benutzerschnittstelle/komponenten/CustomPainter.java
@@ -0,0 +1,23 @@
+package benutzerschnittstelle.komponenten;
+
+import benutzerschnittstelle.MainFrame;
+
+import java.awt.*;
+
+public class CustomPainter
+{
+    // Inspiration by:
+    // Discussion from: https://stackoverflow.com/questions/27706197/how-can-i-center-graphics-drawstring-in-java
+    // Answer here: https://stackoverflow.com/a/27740330/16376071
+    public static void drawTitle(Component component, Graphics g, String title)
+    {
+        // Get the FontMetrics
+        FontMetrics metrics = g.getFontMetrics(MainFrame.liesFont());
+        // Determine the X coordinate for the text
+        int x = (component.getWidth() - metrics.stringWidth(title)) / 2;
+        // Determine the Y coordinate for the text (note we add the ascent, as in java 2d 0 is top of the screen)
+        int y = (component.getHeight() - metrics.getHeight()) / 2 + metrics.getAscent();
+        // Draw the String
+        g.drawString(title, x, y);
+    }
+}
---
+++ src/benutzerschnittstelle/komponenten/charts/LineChart.java
@@ -0,0 +1,7 @@
+package benutzerschnittstelle.komponenten.charts;
+
+import javax.swing.*;
+
+public class LineChart extends JPanel
+{
+}
