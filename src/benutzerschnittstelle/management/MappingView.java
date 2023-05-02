d4015fcae36781460ad64dfa35bbed153f93710a
Julian Schumacher <Jules.media@outlook.de>
2023-05-02 22:16:53 +0200

    Update dimensions

--- src/benutzerschnittstelle/management/MappingView.java
+++ src/benutzerschnittstelle/management/MappingView.java
@@ -148,13 +148,13 @@ public class MappingView extends CustomPanel
         panel.setLayout(layout);
         constraints.gridx = 0;
         constraints.gridy = 0;
-        constraints.gridwidth = 5;
+        constraints.gridwidth = 8;
         panel.add(vokPane, constraints);
-        constraints.gridx = 6;
+        constraints.gridx = 9;
         constraints.gridwidth = 1;
         panel.add(new JPanel(), constraints);
-        constraints.gridwidth = 5;
-        constraints.gridx = 7;
+        constraints.gridwidth = 8;
+        constraints.gridx = 10;
         panel.add(katPane, constraints);
         return panel;
     }
