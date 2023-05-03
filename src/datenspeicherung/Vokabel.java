985a7980ee3f7520caf558d440d1c462be13fee6
NickRTR <95316598+NickRTR@users.noreply.github.com>
2023-05-03 10:59:53 +0200

    Adaptive Vokabelpräsentation #10

--- src/datenspeicherung/Vokabel.java
+++ src/datenspeicherung/Vokabel.java
@@ -1,78 +1,87 @@
 package datenspeicherung;
 
-public final class Vokabel
+public final class Vokabel implements Comparable<Vokabel>
 {
-    // Vokabel-Datentyp
-    private final String wort;
-    private final String uebersetzung;
-    private final byte[] abbildung;
-    private final byte[] aussprache;
-    private final String lautschrift;
-    private final String verwendungshinweis;
-    private int anzahlrichtig;
-    private int wiederholungen;
+	// Vokabel-Datentyp
+	private final String wort;
+	private final String uebersetzung;
+	private final byte[] abbildung;
+	private final byte[] aussprache;
+	private final String lautschrift;
+	private final String verwendungshinweis;
+	private int anzahlrichtig;
+	private int wiederholungen;
 
-    public Vokabel(String pWort, String pUebersetzung, byte[] pAbbildung, byte[] pAussprache, String pLautschrift, String pVerwendungshinweis, int pWiederholungen, int pAnzahlrichtig)
-    {
-        wort = pWort;
-        uebersetzung = pUebersetzung;
-        abbildung = pAbbildung;
-        aussprache = pAussprache;
-        lautschrift = pLautschrift;
-        verwendungshinweis = pVerwendungshinweis;
-        wiederholungen = pWiederholungen;
-        anzahlrichtig = pAnzahlrichtig;
-    }
+	public Vokabel(String pWort, String pUebersetzung, byte[] pAbbildung,
+			byte[] pAussprache, String pLautschrift, String pVerwendungshinweis,
+			int pWiederholungen, int pAnzahlrichtig)
+	{
+		wort = pWort;
+		uebersetzung = pUebersetzung;
+		abbildung = pAbbildung;
+		aussprache = pAussprache;
+		lautschrift = pLautschrift;
+		verwendungshinweis = pVerwendungshinweis;
+		wiederholungen = pWiederholungen;
+		anzahlrichtig = pAnzahlrichtig;
+	}
 
-    public String liesWort()
-    {
-        return wort;
-    }
+	public String liesWort()
+	{
+		return wort;
+	}
 
-    public String liesUebersetzung()
-    {
-        return uebersetzung;
-    }
+	public String liesUebersetzung()
+	{
+		return uebersetzung;
+	}
 
-    public byte[] liesAbbildung()
-    {
-        return abbildung;
-    }
+	public byte[] liesAbbildung()
+	{
+		return abbildung;
+	}
 
-    public byte[] liesAussprache()
-    {
-        return aussprache;
-    }
+	public byte[] liesAussprache()
+	{
+		return aussprache;
+	}
 
-    public String liesLautschrift()
-    {
-        return lautschrift;
-    }
+	public String liesLautschrift()
+	{
+		return lautschrift;
+	}
 
-    public String liesVerwendungshinweis()
-    {
-        return verwendungshinweis;
-    }
+	public String liesVerwendungshinweis()
+	{
+		return verwendungshinweis;
+	}
 
-    public int liesWiederholungen()
-    {
-        return wiederholungen;
-    }
+	public int liesWiederholungen()
+	{
+		return wiederholungen;
+	}
 
-    public int liesAnzahlRichtig()
-    {
-        return anzahlrichtig;
-    }
+	public int liesAnzahlRichtig()
+	{
+		return anzahlrichtig;
+	}
 
-    public long liesQuote()
-    {
-        return Math.round(((double) anzahlrichtig / wiederholungen * 100) * 100) / 100;
-    }
+	public int liesQuote()
+	{
+		return (int) (Math.round(
+				((double) anzahlrichtig / wiederholungen * 100) * 100) / 100);
+	}
 
-    public void wiederholt(boolean richtig)
-    {
-        wiederholungen++;
-        if (richtig)
-            anzahlrichtig++;
-    }
+	public void wiederholt(boolean richtig)
+	{
+		wiederholungen++;
+		if (richtig)
+			anzahlrichtig++;
+	}
+
+	@Override
+	public int compareTo(Vokabel v)
+	{
+		return Integer.compare(liesQuote(), v.liesQuote());
+	}
 }
