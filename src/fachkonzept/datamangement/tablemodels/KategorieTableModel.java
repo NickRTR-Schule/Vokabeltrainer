f4298e1a156eaed8aefaca8194a57b8bbeccda0c
Julian Schumacher <professional@julianschumacher.dev>
2023-06-19 21:42:14 +0200

    Revert "Delete current mapping view"
    This reverts commit d68316b5c370aa9c87244af284ab3578c975dcfe.

---
+++ src/fachkonzept/datamangement/tablemodels/KategorieTableModel.java
@@ -0,0 +1,36 @@
+package fachkonzept.datamangement.tablemodels;
+
+import datenspeicherung.Kategorie;
+
+public final class KategorieTableModel extends CustomTableModel<Kategorie>
+{
+
+    private static final int COLUMN_NAME = 0;
+    private static final int COLUMN_VOKSANZAHL = 1;
+
+    public KategorieTableModel()
+    {
+        super(
+                new String[]{
+                        "Name",
+                        "Anzahl Vokabeln"
+                },
+                new Class<?>[]{
+                        String.class,
+                        Integer.class
+                }
+        );
+    }
+
+    @Override
+    public Object getValueAt(int row, int column)
+    {
+        final Kategorie kategorie = rows.get(row);
+        return switch (column)
+        {
+            case COLUMN_NAME -> kategorie.liesName();
+            case COLUMN_VOKSANZAHL -> kategorie.liesVokabeln().size();
+            default -> null;
+        };
+    }
+}
