1980af6a7304c8554e76ffe80f7a6a0c06e656d6
NickRTR <95316598+NickRTR@users.noreply.github.com>
2023-06-21 10:11:37 +0200

    Shortcut Buttons hinzugefügt

--- src/benutzerschnittstelle/management/ListView.java
+++ src/benutzerschnittstelle/management/ListView.java
@@ -1,114 +1,123 @@
 package benutzerschnittstelle.management;
 
-import benutzerschnittstelle.komponenten.CustomPanel;
-import fachkonzept.datamangement.tablemodels.CustomTableModel;
-import steuerung.MainFrameSteuerung;
-import steuerung.management.ListenSteuerung;
-
-import javax.swing.*;
-import javax.swing.table.TableModel;
-import java.awt.*;
+import java.awt.Color;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.GridLayout;
+import java.awt.Insets;
 import java.util.ArrayList;
 
-public class ListView<T> extends CustomPanel
-{
+import javax.swing.BorderFactory;
+import javax.swing.JButton;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+import javax.swing.RowSorter;
+import javax.swing.ScrollPaneLayout;
+import javax.swing.table.TableModel;
 
-    public final JTable table;
-    private final CustomTableModel<T> model;
-    private final ListenSteuerung<T> steuerung;
-    private ArrayList<T> objects;
+import benutzerschnittstelle.komponenten.CustomPanel;
+import fachkonzept.datamangement.tablemodels.CustomTableModel;
+import steuerung.DashboardSteuerung;
+import steuerung.MainFrameSteuerung;
+import steuerung.management.ListenSteuerung;
+import steuerung.management.VokabellisteSteuerung;
 
-    public ListView(String name, CustomTableModel<T> model, ListenSteuerung<T> steuerung)
-    {
-        super("Vokabeln");
-        this.steuerung = steuerung;
-        try
-        {
-            objects = steuerung.liesDaten();
-        } catch (Exception ignored)
-        {
-            objects = new ArrayList<>();
-            JOptionPane.showMessageDialog(this, "Fehler beim laden der Daten");
-        }
-        this.model = model;
-        table = new JTable(model);
-        init();
-    }
+public class ListView<T> extends CustomPanel {
 
-    private void init()
-    {
-        setBackground(Color.WHITE);
-        add(build());
-        setName(getTitle());
-    }
+	public final JTable table;
+	private final CustomTableModel<T> model;
+	private final ListenSteuerung<T> steuerung;
+	private ArrayList<T> objects;
 
-    private JPanel build()
-    {
-        final JPanel panel = new JPanel();
-        final GridBagConstraints constraints = new GridBagConstraints();
-        constraints.fill = GridBagConstraints.BOTH;
-        constraints.anchor = GridBagConstraints.CENTER;
-        constraints.insets = new Insets(5, 5, 5, 5);
-        final GridBagLayout layout = new GridBagLayout();
-        layout.setConstraints(this, constraints);
-        panel.setLayout(layout);
-        constraints.gridx = 1;
-        constraints.gridy = 0;
-        constraints.weightx = 1;
-        constraints.weighty = 1;
-        panel.add(getTable(), constraints);
-        constraints.weightx = 0;
-        constraints.weighty = 0;
-        constraints.gridy = 1;
-        panel.add(getActionPanel(), constraints);
-        return panel;
-    }
+	public ListView(String name, CustomTableModel<T> model, ListenSteuerung<T> steuerung) {
+		super("Vokabeln");
+		this.steuerung = steuerung;
+		try {
+			objects = steuerung.liesDaten();
+		} catch (Exception ignored) {
+			objects = new ArrayList<>();
+			JOptionPane.showMessageDialog(this, "Fehler beim laden der Daten");
+		}
+		this.model = model;
+		table = new JTable(model);
+		init();
+	}
 
-    private JScrollPane getTable()
-    {
-        table.setDragEnabled(false);
-        table.getTableHeader().setReorderingAllowed(false);
-        for (final T obj : objects)
-        {
-            model.addRow(obj);
+	private void init() {
+		setBackground(Color.WHITE);
+		add(build());
+		setName(getTitle());
+	}
 
-        }
-        final JScrollPane pane = new JScrollPane();
-        pane.setLayout(new ScrollPaneLayout());
-        pane.setBorder(BorderFactory.createEmptyBorder());
-        pane.setViewportView(table);
-        return pane;
-    }
+	private JPanel build() {
+		final JPanel panel = new JPanel();
+		final GridBagConstraints constraints = new GridBagConstraints();
+		constraints.fill = GridBagConstraints.BOTH;
+		constraints.anchor = GridBagConstraints.CENTER;
+		constraints.insets = new Insets(5, 5, 5, 5);
+		final GridBagLayout layout = new GridBagLayout();
+		layout.setConstraints(this, constraints);
+		panel.setLayout(layout);
+		constraints.gridx = 1;
+		constraints.gridy = 0;
+		constraints.weightx = 1;
+		constraints.weighty = 1;
+		panel.add(getTable(), constraints);
+		constraints.weightx = 0;
+		constraints.weighty = 0;
+		constraints.gridy = 1;
+		panel.add(getActionPanel(), constraints);
+		return panel;
+	}
 
-    private JPanel getActionPanel()
-    {
-        final JPanel panel = new JPanel();
-        panel.setLayout(new GridLayout(1, 4));
-        final JButton showBtn = new JButton("Show");
-        showBtn.addActionListener((ignored) -> MainFrameSteuerung.getInstance().openUebersicht(getCurrentObject()));
-        panel.add(showBtn);
-        final JButton deleteBtn = new JButton("Delete");
-        deleteBtn.addActionListener((ignored) -> {
-            try
-            {
-                final T obj = getCurrentObject();
-                steuerung.loescheDatensatz(obj);
-                model.removeRow(obj);
-            } catch (Exception ig)
-            {
-                JOptionPane.showMessageDialog(this, "Fehler beim loeschen der Vokabel");
-            }
-            model.fireTableRowsUpdated(0, objects.size());
-            objects.remove(table.getSelectedRow());
-        });
-        panel.add(deleteBtn);
-        return panel;
-    }
+	private JScrollPane getTable() {
+		table.setDragEnabled(false);
+		table.getTableHeader().setReorderingAllowed(false);
+		for (final T obj : objects) {
+			model.addRow(obj);
 
-    private T getCurrentObject()
-    {
-        final int row = table.getSelectedRow();
-        final RowSorter<? extends TableModel> sorter = table.getRowSorter();
-        return model.getObjectForRow(sorter != null ? sorter.convertRowIndexToModel(table.getSelectedRow()) : row);
-    }
+		}
+		final JScrollPane pane = new JScrollPane();
+		pane.setLayout(new ScrollPaneLayout());
+		pane.setBorder(BorderFactory.createEmptyBorder());
+		pane.setViewportView(table);
+		return pane;
+	}
+
+	private JPanel getActionPanel() {
+		final JPanel panel = new JPanel();
+		panel.setLayout(new GridLayout(1, 4));
+		final JButton showBtn = new JButton("Show");
+		showBtn.addActionListener((ignored) -> MainFrameSteuerung.getInstance().openUebersicht(getCurrentObject()));
+		panel.add(showBtn);
+		final JButton addBtn = new JButton("Add");
+		if (steuerung instanceof VokabellisteSteuerung) {
+			addBtn.addActionListener((ignored) -> new DashboardSteuerung().erstellerGeklickt());
+		} else {
+			addBtn.addActionListener((ignored) -> MainFrameSteuerung.getInstance());
+		}
+		panel.add(addBtn);
+		final JButton deleteBtn = new JButton("Delete");
+		deleteBtn.addActionListener((ignored) -> {
+			try {
+				final T obj = getCurrentObject();
+				steuerung.loescheDatensatz(obj);
+				model.removeRow(obj);
+			} catch (Exception ig) {
+				JOptionPane.showMessageDialog(this, "Fehler beim loeschen der Vokabel");
+			}
+			model.fireTableRowsUpdated(0, objects.size());
+			objects.remove(table.getSelectedRow());
+		});
+		panel.add(deleteBtn);
+		return panel;
+	}
+
+	private T getCurrentObject() {
+		final int row = table.getSelectedRow();
+		final RowSorter<? extends TableModel> sorter = table.getRowSorter();
+		return model.getObjectForRow(sorter != null ? sorter.convertRowIndexToModel(table.getSelectedRow()) : row);
+	}
 }
--- src/steuerung/MainFrameSteuerung.java
+++ src/steuerung/MainFrameSteuerung.java
@@ -1,156 +1,141 @@
 package steuerung;
 
+import java.awt.EventQueue;
+
 import benutzerschnittstelle.MainFrame;
 import benutzerschnittstelle.navigation.UIScreens;
 import datenspeicherung.Kategorie;
 import datenspeicherung.Vokabel;
 import fachkonzept.navigation.NavigationStack;
 
-import java.awt.*;
-
 /**
  * The Main Frame Controller controlling the Main Frame and with that Navigation
  * of this App.
  */
-public final class MainFrameSteuerung
-{
+public final class MainFrameSteuerung {
 
-    /**
-     * The singleton Object to access this Controller everywhere in the App
-     */
-    private static MainFrameSteuerung shared;
+	/**
+	 * The singleton Object to access this Controller everywhere in the App
+	 */
+	private static MainFrameSteuerung shared;
 
-    /**
-     * The Main Frame of this Application handling all the other Screens as
-     * content Panes of this single Main Frame.
-     * <p>
-     * WARNING: Only one of this Main Frame should exist in a single Application
-     * run
-     */
-    private final MainFrame mainFrame;
+	/**
+	 * The Main Frame of this Application handling all the other Screens as content
+	 * Panes of this single Main Frame.
+	 * <p>
+	 * WARNING: Only one of this Main Frame should exist in a single Application run
+	 */
+	private final MainFrame mainFrame;
 
-    private MainFrameSteuerung()
-    {
-        mainFrame = new MainFrame();
-    }
+	private MainFrameSteuerung() {
+		mainFrame = new MainFrame();
+	}
 
-    /**
-     * Launch the application.
-     */
-    public static void main(String[] args)
-    {
-        EventQueue.invokeLater(() -> shared = new MainFrameSteuerung());
-    }
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(() -> shared = new MainFrameSteuerung());
+	}
 
-    public static MainFrameSteuerung getInstance()
-    {
-        return shared;
-    }
+	public static MainFrameSteuerung getInstance() {
+		return shared;
+	}
 
-    private void openForward(UIScreens screen)
-    {
-        NavigationStack.getInstance().forward(screen);
-    }
+	private void openForward(UIScreens screen) {
+		NavigationStack.getInstance().forward(screen);
+	}
 
-    private void openForward(UIScreens screen, Object obj)
-    {
-        NavigationStack.getInstance().forward(screen, obj);
-    }
+	private void openForward(UIScreens screen, Object obj) {
+		NavigationStack.getInstance().forward(screen, obj);
+	}
 
-    /**
-     * Navigates to the Dashboard of this App
-     */
-    public void openDashboard()
-    {
-        final UIScreens dashboard = UIScreens.Dashboard;
-        openForward(dashboard);
-        mainFrame.open(dashboard);
-    }
+	/**
+	 * Navigates to the Dashboard of this App
+	 */
+	public void openDashboard() {
+		final UIScreens dashboard = UIScreens.Dashboard;
+		openForward(dashboard);
+		mainFrame.open(dashboard);
+	}
 
-    /**
-     * Navigates to the Vokabel Abfrage Screen of this App
-     */
-    public void openAbfrage()
-    {
-        final UIScreens screen = UIScreens.Abfrage;
-        openForward(screen);
-        mainFrame.open(screen);
-    }
+	/**
+	 * Navigates to the Vokabel Abfrage Screen of this App
+	 */
+	public void openAbfrage() {
+		final UIScreens screen = UIScreens.Abfrage;
+		openForward(screen);
+		mainFrame.open(screen);
+	}
 
-    public void openUebersicht(Object obj)
-    {
-        if (obj instanceof Vokabel)
-        {
-            openVokabeluebersicht((Vokabel) obj);
-        } else if (obj instanceof Kategorie)
-        {
-            openKategorieuebersicht((Kategorie) obj);
-        } else
-        {
-            throw new IllegalArgumentException();
-        }
-    }
+	public void openUebersicht(Object obj) {
+		if (obj instanceof Vokabel) {
+			openVokabeluebersicht((Vokabel) obj);
+		} else if (obj instanceof Kategorie) {
+			openKategorieuebersicht((Kategorie) obj);
+		} else {
+			throw new IllegalArgumentException();
+		}
+	}
 
-    /**
-     * WARNING!
-     * Only use in Navigation Stack
-     *
-     * @param screen
-     * @param obj
-     */
-    public void open(UIScreens screen, Object obj)
-    {
-        mainFrame.open(screen, obj);
-    }
+	/**
+	 * WARNING! Only use in Navigation Stack
+	 *
+	 * @param screen
+	 * @param obj
+	 */
+	public void open(UIScreens screen, Object obj) {
+		mainFrame.open(screen, obj);
+	}
 
-    /**
-     * Navigates to the KategorieScreen of this App
-     */
-    private void openKategorieuebersicht(Kategorie kategorie)
-    {
-        final UIScreens screen = UIScreens.Kategorieeuebersicht;
-        openForward(screen, kategorie);
-        mainFrame.open(screen, kategorie);
-    }
+	/**
+	 * Navigates to the KategorieScreen of this App
+	 */
+	private void openKategorieuebersicht(Kategorie kategorie) {
+		final UIScreens screen = UIScreens.Kategorieeuebersicht;
+		openForward(screen, kategorie);
+		mainFrame.open(screen, kategorie);
+	}
 
-    private void openVokabeluebersicht(Vokabel vokabel)
-    {
-        final UIScreens screen = UIScreens.Vokabeluebersicht;
-        openForward(screen, vokabel);
-        mainFrame.open(screen, vokabel);
-    }
+	private void openKategorieuebersicht() {
+		final UIScreens screen = UIScreens.Kategorieeuebersicht;
+		openForward(screen);
+		mainFrame.open(screen);
+	}
 
-    /**
-     * Navigates to the Vokabel Ersteller of this App
-     */
-    public void openErsteller()
-    {
-        final UIScreens screen = UIScreens.Ersteller;
-        openForward(screen);
-        mainFrame.open(screen);
-    }
+	private void openVokabeluebersicht(Vokabel vokabel) {
+		final UIScreens screen = UIScreens.Vokabeluebersicht;
+		openForward(screen, vokabel);
+		mainFrame.open(screen, vokabel);
+	}
 
-    public void openVokabelliste()
-    {
-        final UIScreens screen = UIScreens.Vokabelliste;
-        openForward(screen);
-        mainFrame.open(screen);
-    }
+	/**
+	 * Navigates to the Vokabel Ersteller of this App
+	 */
+	public void openErsteller() {
+		final UIScreens screen = UIScreens.Ersteller;
+		openForward(screen);
+		mainFrame.open(screen);
+	}
 
-    public void openKategorieliste()
-    {
-        final UIScreens screen = UIScreens.Kategorieliste;
-        openForward(screen);
-        mainFrame.open(screen);
-    }
+	public void openVokabelliste() {
+		final UIScreens screen = UIScreens.Vokabelliste;
+		openForward(screen);
+		mainFrame.open(screen);
+	}
 
-    /**
-     * Navigates to the Statistics Screen of this App
-     */
-    public void openStats()
-    {
-        final UIScreens screen = UIScreens.Statistik;
-        openForward(screen);
-        mainFrame.open(screen);
-    }
+	public void openKategorieliste() {
+		final UIScreens screen = UIScreens.Kategorieliste;
+		openForward(screen);
+		mainFrame.open(screen);
+	}
+
+	/**
+	 * Navigates to the Statistics Screen of this App
+	 */
+	public void openStats() {
+		final UIScreens screen = UIScreens.Statistik;
+		openForward(screen);
+		mainFrame.open(screen);
+	}
 }
